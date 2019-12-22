c036ca734b838de656cbe7f8039e7027a409be47
Greyson Jennings <tallesttower1@gmail.com>
2019-12-22 17:31:21 +0100

    commented debugging thing. now to think of a solution .... .. ..

--- raytracer.py
+++ raytracer.py
@@ -12,8 +12,8 @@ debug = Image.new("RGB",(window["w"],window["h"]))
 debugRaw = debug.load()
 #define vectors needed
 lowerLeftCorner = Vector((window['w']/100)*-1,(window['h']/100)*-1,-1)
-hAxis = Vector((window['w']/50),0,0)
-vAxis = Vector(0,(window['h']/50),0)
+hAxis = Vector((window['w']/50),0,0) #I don't remmeber what H means. The x axis that the ray directio lerps across.
+vAxis = Vector(0,(window['h']/50),0) #This is the y axis that it lerps across.
 Origin = Vector(0,0,0)
 #temporarily hardcoded spheres
 hittableList = [Sphere(Vector(0,0,-1),.5),Sphere(Vector(2,-1,-2),.5),Sphere(Vector(-2,0,-2),.5)]
@@ -24,18 +24,28 @@ for x in range(window["w"]):
         u = (x/window["w"])
         v = (y/window["h"])
         #build ray
-        ray = Ray(Origin,.5*(lowerLeftCorner+(hAxis*(u))+(vAxis*v)).unit())
+        #With my debugging trials, I can deduce that the problem *might be* with this line here.
+        #what its doing is taking the left corner, and then moving the ray by how much it has gone left and down.
+        ray = Ray(Origin,(lowerLeftCorner+(hAxis*u)+(vAxis*v)).unit())
         
         #get pixel color
         col = color(ray,hittableList)
         #turn color into something readable by PIL (255-base tuple)
         o = (col*255).to_list()
         o = [int(x) for x in o]
-        if u == v:
-            d = (255,255,255)
+        #Gen Debug image
+        #Ok yes,  the debug image looks like a hot mess, I know.
+        #How to read it:
+        #Colors are your basic UV "hello World".
+        #The white line is to see if the u and v are iterating linearly. if it is straight, they are.
+        #The horrble blue thing is, if there is no distortion in the image, a grid of uniform thickness.
+        #however, you will  see that it is not. It is, instead, pinched at the middle.
+        #Here, we can see that the problem is the vector, and not, in fact, the UVs.
+        if u == v: 
+            d = (255,255,255) #draw white line
         elif int((ray.dir.x/hAxis.x)*100) % int((5/window['w'])*100) == 0 or int((ray.dir.y/vAxis.y)*100) % int((5/window['h'])*100) == 0:
-            d = (0,0,255)
-        else:
+            d = (0,0,255) #Draw blue grid
+        else: #draw hello world
             d = (int(abs(u*255)),int(abs(v*255)),int(.2*255))
         #set px color
         raw[x,y] = tuple(o)
